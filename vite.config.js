ed1ad877b713be2a8cd55bfc77a06690dda37f21
Han Wang <hanforwork896@gmail.com>
2023-11-29 13:43:04 +0800

    fix: add base in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/2023-f2e-02/',
   plugins: [
     vue(),
   ],
@@ -12,5 +13,11 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  // build: {
+  //   rollupOptions: {
+  //     input: ['src/main.js', './index.html']
+  //   },
+  //   outDir: 'dist',
+  // },
 });

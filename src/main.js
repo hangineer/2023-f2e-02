6d8c88415443c2056c062d691181dab8d78b16d4
Han Wang <hanforwork896@gmail.com>
2023-11-29 10:29:10 +0800

    build: install axios, chart.js

--- src/main.js
+++ src/main.js
@@ -1,18 +1,20 @@
 import './assets/main.css';
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { Tab, Select, initTE } from "tw-elements";
-
-initTE({ Tab, Select });
-
 import App from './App.vue';
 import router from './router';
+
+initTE({ Tab, Select });
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VueAxios, axios);
 
 app.component('faIcon', FontAwesomeIcon);
 
